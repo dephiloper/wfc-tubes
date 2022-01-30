9c40f3118cf285e9e419bcf23b258a89a64027e8
Philipp Bönsch <philipp@PHILIPPS-MBP.local>
2022-01-30 05:51:33 +0100

    Configure public path.

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,9 +1,11 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { resolve } = require('path');
 const { merge } = require('webpack-merge');
+const { DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const commonConfig = require('.');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ASSET_PATH = process.env.ASSET_PATH || '/wfc-tubes';
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -11,10 +13,13 @@ module.exports = merge(commonConfig, {
   output: {
     filename: 'js/bundle.[fullhash].min.js',
     path: resolve(__dirname, '../../dist'),
-    publicPath: '/',
+    publicPath: ASSET_PATH
   },
   devtool: 'source-map',
   plugins: [
+    new DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    }),
     new CopyWebpackPlugin({
       patterns: [
         {
